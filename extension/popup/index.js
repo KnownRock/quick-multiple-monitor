82c7ff38e16c404852d8f48e99e37b5e241897a6
knownrock <knownrock@hotmail.com>
2022-03-02 10:36:43 +0800

    [op] using system.display to get the screen info

--- extension/popup/index.js
+++ extension/popup/index.js
@@ -1,84 +1,48 @@
 !(function () {
-  const textarea = document.getElementById("textarea");
-  const btn = document.getElementById("btn");
-  const resetButton = document.getElementById("reset")
-
   const widthSlider = document.getElementById("width");
   const heightSlider = document.getElementById("height");
 
   const widthLabel = document.getElementById("width-label");
   const heightLabel = document.getElementById("height-label");
 
-  chrome.storage.sync.get("allScreensJson", function (data) {
-    textarea.value = data && data.allScreensJson || '[\n  { "x": 0, "y": 0, "width": 1920, "height": 1080 }\n]';
-  })
+  const saveBtn = document.getElementById("btn");
+  const resetBtn = document.getElementById("reset")
+
+  function onSliderValueChanged(){
+    widthLabel.innerText = widthSlider.value + 'px'
+    heightLabel.innerText = heightSlider.value + 'px'
+  }
 
   chrome.storage.sync.get("size", function (data) {
     widthSlider.value = data && data.size && data.size.width || 320
     heightSlider.value = data && data.size && data.size.height || 180
-
-    widthLabel.innerText = widthSlider.value + 'px'
-    heightLabel.innerText = heightSlider.value + 'px'
+    onSliderValueChanged()
   })
 
   widthSlider.addEventListener("input", function () {
-    widthLabel.innerText = widthSlider.value + "px"
+    onSliderValueChanged()
   })
 
   heightSlider.addEventListener("input", function () {
-    heightLabel.innerText = heightSlider.value + "px"
+    onSliderValueChanged()
+  })
+
+  resetBtn.addEventListener("click", function () {
+    widthSlider.value = 320
+    heightSlider.value = 180
+    onSliderValueChanged()
   })
 
 
-
-  resetButton.addEventListener("click", function () {
-    textarea.value = '[\n  { "x": 0, "y": 0, "width": 1920, "height": 1080 }\n]';
-  })
-
-
-  btn.addEventListener("click", () => {
-    const data = textarea.value;
-    console.log(data);
-    // alert(data)
-    let objs = [];
-    try {
-      objs = JSON.parse(data);
-    } catch (e) {
-      alert('invalid json');
-    }
-
-    const allScreens = objs
-    const isValid = allScreens.every(screen => {
-      if (screen.x === null || screen.x === undefined &&
-        screen.y === null || screen.y === undefined &&
-        screen.width === null || screen.width === undefined &&
-        screen.height === null || screen.height === undefined) {
-        alert('need x, y, width, height in each screen');
-        return false;
-      }
-      return true;
-    });
-
-    if (!isValid) {
-      return;
-    }
-
-
+  saveBtn.addEventListener("click", () => {
     chrome.storage.sync.set({
-      allScreensJson: data
+      size: {
+        width: widthSlider.value,
+        height: heightSlider.value
+      }
     }, function () {
-
-      chrome.storage.sync.set({
-        size: {
-          width: widthSlider.value,
-          height: heightSlider.value
-        }
-      }, function () {
-        alert('saved');
-      })
-    });
-
-
+      alert('saved');
+    })
   });
 })()
 
